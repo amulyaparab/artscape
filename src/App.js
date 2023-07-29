4d731d143379c767a487901592c3f6cddbc79c75
Amulya Parab <amulyaparab101@gmail.com>
2023-07-29 15:28:24 +0530

    feat: added navbar

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import { Videos } from "./Pages/Videos/Videos";
 import { WatchLater } from "./Pages/WatchLater";
 import { SingleVideo } from "./Pages/SingleVideo/SingleVideo";
 import { SinglePlaylist } from "./Pages/SinglePlaylist";
+import { Navbar } from "./Components/Navbar/Navbar";
 function App() {
   return (
-    <div className="App page-divider">
-      <SideNav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/allPlaylists" element={<Playlists />} />
-        <Route path="/videos/:categoryName" element={<Videos />} />
-        <Route path="/watchLater" element={<WatchLater />} />
-        <Route path="/singlePlaylist" element={<SinglePlaylist />} />
-        <Route path="/singleVideo/:videoId" element={<SingleVideo />} />
-      </Routes>
-    </div>
+    <>
+      <Navbar />
+      <div className="App page-divider">
+        <SideNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/allPlaylists" element={<Playlists />} />
+          <Route path="/videos/:categoryName" element={<Videos />} />
+          <Route path="/watchLater" element={<WatchLater />} />
+          <Route path="/singlePlaylist" element={<SinglePlaylist />} />
+          <Route path="/singleVideo/:videoId" element={<SingleVideo />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import "./navbar.css";
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>ArtScape</h1>
+    </nav>
+  );
+};
